74e43142e6a778a2a1846d01ff75d0d46738ca5f
Shamia702 <shamiakhaliq09@gmail.com>
2026-01-25 10:54:31 +0100

    fixed the supabase cdn link.

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.49.1/+esm";
 
 const supabaseUrl = "https://ftswvmbxqunmfnzgvvbd.supabase.co";
 const supabaseAnonKey = "sb_publishable_yLEp5elTfhip50BUofCNSg_J-3syD0l";
